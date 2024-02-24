0fd1c89cdce824be29daf4e638986128b36bdd45
Caio <145506559+CaioWired@users.noreply.github.com>
2024-02-23 23:23:36 -0300

    Update nomeSobrenome.c

--- nomeSobrenome.c
+++ nomeSobrenome.c
@@ -4,9 +4,9 @@ int main(){
 	char nome[12];
 	char sobrenome[12];
 	char nomeCompleto[24];
-	printf("Qual o teu nome? ");
+	printf("Qual o teu nome?\n");
 	gets(nome);
-	printf("Qual o teu sobrenome? ");
+	printf("Qual o teu sobrenome?\n");
 	gets(sobrenome);
 	printf(nome);
 	printf(" ");
