ef45274160ad181eb7ded5b3e901a3ce7822c530
Caio <145506559+CaioWired@users.noreply.github.com>
2024-03-07 10:42:24 -0300

    Add files via upload

---
+++ nomeIdadeAlturaResultado.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int idade;
+	char letraInicial;
+	float altura;
+	
+	printf("Digite idade: \n");
+	scanf("%d", &idade);
+	printf("A letra inicial do teu nome: \n");
+    scanf(" %c", &letraInicial);
+	printf("A tua altura: \n");
+	scanf("%f", &altura);
+	system("cls");
+	printf("Tua idade: \n");
+	printf("%d \n", idade);
+	printf("Letra Inicial: \n");
+	printf(" %c \n", letraInicial);
+	printf("Altura: \n");
+	printf("%f \n", altura);
+	
+	return 1;
+}
